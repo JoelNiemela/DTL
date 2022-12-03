72d57e1172e84c8d8fb0353cbb5e658e68ad121e
JoelNiemela <joel@niemela.se>
2022-12-02 19:05:59 -0600

    Use spaces for indentation

--- dtl/cli.py
+++ dtl/cli.py
@@ -9,291 +9,291 @@ from dtl.config import load_config
 
 from collections import defaultdict
 
-VERSION = 'v0.1.7-alpha'
+VERSION = 'v0.1.8-alpha'
 
 config = load_config(os.path.expanduser('~/.config/DTL/config.ini'))
 DTL_dir = config['DTL_dir']
 
 def assert_argc(args: list[str], count: int) -> None:
-	if len(args) < count:
-		if count == 1:
-			print('Error: expected an argument')
-		else:
-			print(f'Error: expected {count} arguments')
-		exit(1)
+    if len(args) < count:
+        if count == 1:
+            print('Error: expected an argument')
+        else:
+            print(f'Error: expected {count} arguments')
+        exit(1)
 
 def require(name: str, value) -> None:
-	if value is None:
-		print(f'Error: argument "{name}" is required')
-		exit(1)
+    if value is None:
+        print(f'Error: argument "{name}" is required')
+        exit(1)
 
 def absolute_path(file_path: str) -> str:
-	if len(file_path) > 0 and file_path[0] == '@':
-		return f'{DTL_dir}/{file_path[1:]}.dtl'
+    if len(file_path) > 0 and file_path[0] == '@':
+        return f'{DTL_dir}/{file_path[1:]}.dtl'
 
-	return file_path
+    return file_path
 
 def parse_file(file_path: str|None) -> ast.File:
-	require('file', file_path)
+    require('file', file_path)
 
-	parser = Parser(debug = False)
+    parser = Parser(debug = False)
 
-	try:
-		with open(absolute_path(file_path), 'r') as file:
-			tree = parser.parse(file.read())
-	except FileNotFoundError:
-		print(f'Error: can\'t find file "{file_path}"')
-		exit(1)
+    try:
+        with open(absolute_path(file_path), 'r') as file:
+            tree = parser.parse(file.read())
+    except FileNotFoundError:
+        print(f'Error: can\'t find file "{file_path}"')
+        exit(1)
 
-	return tree
+    return tree
 
 def write_file(file_path: str|None, tree: ast.File) -> None:
-	require('file', file_path)
+    require('file', file_path)
 
-	with open(absolute_path(file_path), 'w') as file:
-		file.write(tree.format())
+    with open(absolute_path(file_path), 'w') as file:
+        file.write(tree.format())
 
 def parse_cmd(file_path: str|None, args: list[str]) -> None:
-	require('file', file_path)
+    require('file', file_path)
 
-	print(parse_file(file_path))
-	print(parse_file(file_path).format())
+    print(parse_file(file_path))
+    print(parse_file(file_path).format())
 
 def format_cmd(file_path: str|None, args: list[str]) -> None:
-	require('file', file_path)
+    require('file', file_path)
 
-	tree = parse_file(file_path)
+    tree = parse_file(file_path)
 
-	write_file(file_path, tree)
+    write_file(file_path, tree)
 
 def find_cmd(file_path: str|None, args: list[str]) -> None:
-	require('file', file_path)
+    require('file', file_path)
 
-	assert_argc(args, 1)
+    assert_argc(args, 1)
 
-	if args[0] == 'ongoing':
-		ongoing = True
-		description = args[1]
-	elif args[0] == 'static':
-		ongoing = False
-		description = args[1]
-	else:
-		ongoing = None
-		description = args[0]
+    if args[0] == 'ongoing':
+        ongoing = True
+        description = args[1]
+    elif args[0] == 'static':
+        ongoing = False
+        description = args[1]
+    else:
+        ongoing = None
+        description = args[0]
 
-	tree = parse_file(file_path)
+    tree = parse_file(file_path)
 
-	print(''.join([f.format(ast.Time({})) for f in tree.find(description, ongoing=ongoing)]))
+    print(''.join([f.format(ast.Time({})) for f in tree.find(description, ongoing=ongoing)]))
 
 def add_cmd(file_path: str|None, args: list[str]) -> None:
-	require('file', file_path)
+    require('file', file_path)
 
-	assert_argc(args, 1)
+    assert_argc(args, 1)
 
-	description = args[0]
+    description = args[0]
 
-	tree = parse_file(file_path)
+    tree = parse_file(file_path)
 
-	print(ast.Time.now())
-	tree.insert_segment(ast.Segment(ast.Time.now(), description))
+    print(ast.Time.now())
+    tree.insert_segment(ast.Segment(ast.Time.now(), description))
 
-	print(tree.format())
+    print(tree.format())
 
-	write_file(file_path, tree)
+    write_file(file_path, tree)
 
 def begin_cmd(file_path: str|None, args: list[str]) -> None:
-	require('file', file_path)
+    require('file', file_path)
 
-	assert_argc(args, 1)
+    assert_argc(args, 1)
 
-	description = args[0]
+    description = args[0]
 
-	tree = parse_file(file_path)
+    tree = parse_file(file_path)
 
-	already_ongoing = tree.find(description, ongoing = True)
-	if len(already_ongoing) > 0:
-		if len(already_ongoing) == 1:
-			print(f'There\'s already an ongoing entry with the name "{description}":')
-		else:
-			print(f'There are already ongoing entries with the name "{description}":')
+    already_ongoing = tree.find(description, ongoing = True)
+    if len(already_ongoing) > 0:
+        if len(already_ongoing) == 1:
+            print(f'There\'s already an ongoing entry with the name "{description}":')
+        else:
+            print(f'There are already ongoing entries with the name "{description}":')
 
-		print()
-		print(''.join(['\t' + f.format(ast.Time({})) for f in already_ongoing]))
-		print('Are you sure you want to create another entry?')
-		ans = input('(y/n): ')
-		if ans != 'y':
-			exit(0)
+        print()
+        print(''.join(['\t' + f.format(ast.Time({})) for f in already_ongoing]))
+        print('Are you sure you want to create another entry?')
+        ans = input('(y/n): ')
+        if ans != 'y':
+            exit(0)
 
-	tree.insert_segment(ast.Segment(ast.Time.now(), description, ongoing = True))
+    tree.insert_segment(ast.Segment(ast.Time.now(), description, ongoing = True))
 
-	print(tree.format())
+    print(tree.format())
 
-	write_file(file_path, tree)
+    write_file(file_path, tree)
 
 def end_cmd(file_path: str|None, args: list[str]) -> None:
-	require('file', file_path)
+    require('file', file_path)
 
-	assert_argc(args, 1)
+    assert_argc(args, 1)
 
-	description = args[0]
+    description = args[0]
 
-	tree = parse_file(file_path)
+    tree = parse_file(file_path)
 
-	finds = tree.find(description, ongoing = True, with_parent = True)
+    finds = tree.find(description, ongoing = True, with_parent = True)
 
-	if len(finds) == 0:
-		print(f'No ongoing entry with the name "{description}"')
-		exit(0)
-	elif len(finds) > 1:
-		print(f'There are multiple ongoing entries with the name "{description}":')
-		print()
-		print(''.join(['\t' + f[0].format(ast.Time({})) for f in finds]))
-		while True:
-			print(f'Which one would you like to end?')
-			index = input(f'[1-{len(finds)}]: ')
-			if index.isnumeric() and int(index) > 0 and int(index) <= len(finds):
-				segment, parent = finds[int(index)-1]
-				break
-			else:
-				if index == 'none':
-					exit(0)
-				print(f'Invalid index "{index}"')
-				print(f'(type "none" to cancle)')
-	else:
-		print(f'Currently ongoing entry "{description}":')
-		print()
-		print(''.join(['\t' + f[0].format(ast.Time({})) for f in finds]))
-		print('Are you sure you want to end this entry?')
-		ans = input('(y/n): ')
-		if ans != 'y':
-			exit(0)
+    if len(finds) == 0:
+        print(f'No ongoing entry with the name "{description}"')
+        exit(0)
+    elif len(finds) > 1:
+        print(f'There are multiple ongoing entries with the name "{description}":')
+        print()
+        print(''.join(['\t' + f[0].format(ast.Time({})) for f in finds]))
+        while True:
+            print(f'Which one would you like to end?')
+            index = input(f'[1-{len(finds)}]: ')
+            if index.isnumeric() and int(index) > 0 and int(index) <= len(finds):
+                segment, parent = finds[int(index)-1]
+                break
+            else:
+                if index == 'none':
+                    exit(0)
+                print(f'Invalid index "{index}"')
+                print(f'(type "none" to cancle)')
+    else:
+        print(f'Currently ongoing entry "{description}":')
+        print()
+        print(''.join(['\t' + f[0].format(ast.Time({})) for f in finds]))
+        print('Are you sure you want to end this entry?')
+        ans = input('(y/n): ')
+        if ans != 'y':
+            exit(0)
 
-		segment, parent = finds[0]
+        segment, parent = finds[0]
 
-	parent_segments = defaultdict(list)
-	for sub_time in parent.segments.keys():
-		for seg in parent.segments[sub_time]:
-			if seg is not segment:
-				parent_segments[sub_time].append(seg)
+    parent_segments = defaultdict(list)
+    for sub_time in parent.segments.keys():
+        for seg in parent.segments[sub_time]:
+            if seg is not segment:
+                parent_segments[sub_time].append(seg)
 
-	parent.segments = parent_segments
+    parent.segments = parent_segments
 
-	segment.ongoing = False
-	segment.time.period = True
-	segment.time.end = ast.Time.now()
+    segment.ongoing = False
+    segment.time.period = True
+    segment.time.end = ast.Time.now()
 
-	tree.insert_segment(segment)
+    tree.insert_segment(segment)
 
-	write_file(file_path, tree)
+    write_file(file_path, tree)
 
 def create_cmd(file_path: str|None, args: list[str]) -> None:
-	require('file', file_path)
+    require('file', file_path)
 
-	real_path = absolute_path(file_path)
+    real_path = absolute_path(file_path)
 
-	if os.path.exists(real_path):
-		print(f'Error: file "{file_path}" already exists.')
-		exit(1)
-	else:
-		with open(real_path, 'w', encoding='utf-8'):
-			pass
+    if os.path.exists(real_path):
+        print(f'Error: file "{file_path}" already exists.')
+        exit(1)
+    else:
+        with open(real_path, 'w', encoding='utf-8'):
+            pass
 
 def help_cmd(file: str|None, cmd: str|None, args: list[str]) -> None:
-	match cmd:
-		case 'parse':
-			print('dtl [file] parse\n')
-			print('\tParse and print parse-tree of the given file.')
-		case 'format':
-			print('dtl [file] format\n')
-			print('\tReformat the given file to conform to the standard style guide.')
-		case 'find':
-			print('dtl [file] find (ongoing|static) [description]\n')
-			print('\tPrints a list of entries in the given file with the given description.')
-			print('\tOnly returns ongoing or static entries with ongoing or static options;')
-			print('\treturns both by default.')
-		case 'add':
-			print('dtl [file] add [description]\n')
-			print('\tAdds an entry to the given file with the given description')
-			print('\tand the current time as its timestamp.')
-		case 'begin':
-			print('dtl [file] begin [description]\n')
-			print('\tSame as dtl add, but marks the entry as ongoing.')
-		case 'end':
-			print('dtl [file] end [description]\n')
-			print('\tCloses an ongoing entry in the given file with the given description.')
-		case 'create':
-			print('dtl [file] create\n')
-			print('\tCreates the file <file>. Useful for creating files in the DTL_dir directory.')
-		case None:
-			print(f'DTL {VERSION}')
-			print()
-			print('Available commands:')
-			print('\tdtl [file] parse')
-			print('\t\tParse and print parse-tree of the given file.\n')
-			print('\tdtl [file] format')
-			print('\t\tReformat the given file to conform to the standard style guide.\n')
-			print('\tdtl [file] find (ongoing|static) [description]')
-			print('\t\tPrints a list of entries in the given file with the given description.')
-			print('\t\tOnly returns ongoing or static entries with ongoing or static options;')
-			print('\t\treturns both by default.\n')
-			print('\tdtl [file] add [description]')
-			print('\t\tAdds an entry to the given file with the given description')
-			print('\t\tand the current time as its timestamp.\n')
-			print('\tdtl [file] begin [description]')
-			print('\t\tSame as dtl add, but marks the entry as ongoing.\n')
-			print('\tdtl [file] end [description]')
-			print('\t\tCloses an ongoing entry in the given file with the given description.\n')
-			print('\tdtl [file] create')
-			print('\t\tCreates the file <file>. Useful for creating files in the DTL_dir directory.')
-			print()
-			print('Flags:')
-			print('\t--help')
-			print('\t\tShow this message.')
-			print('\t\tIf a command is specified: show more information about that command.\n')
+    match cmd:
+        case 'parse':
+            print('dtl [file] parse\n')
+            print('\tParse and print parse-tree of the given file.')
+        case 'format':
+            print('dtl [file] format\n')
+            print('\tReformat the given file to conform to the standard style guide.')
+        case 'find':
+            print('dtl [file] find (ongoing|static) [description]\n')
+            print('\tPrints a list of entries in the given file with the given description.')
+            print('\tOnly returns ongoing or static entries with ongoing or static options;')
+            print('\treturns both by default.')
+        case 'add':
+            print('dtl [file] add [description]\n')
+            print('\tAdds an entry to the given file with the given description')
+            print('\tand the current time as its timestamp.')
+        case 'begin':
+            print('dtl [file] begin [description]\n')
+            print('\tSame as dtl add, but marks the entry as ongoing.')
+        case 'end':
+            print('dtl [file] end [description]\n')
+            print('\tCloses an ongoing entry in the given file with the given description.')
+        case 'create':
+            print('dtl [file] create\n')
+            print('\tCreates the file <file>. Useful for creating files in the DTL_dir directory.')
+        case None:
+            print(f'DTL {VERSION}')
+            print()
+            print('Available commands:')
+            print('\tdtl [file] parse')
+            print('\t\tParse and print parse-tree of the given file.\n')
+            print('\tdtl [file] format')
+            print('\t\tReformat the given file to conform to the standard style guide.\n')
+            print('\tdtl [file] find (ongoing|static) [description]')
+            print('\t\tPrints a list of entries in the given file with the given description.')
+            print('\t\tOnly returns ongoing or static entries with ongoing or static options;')
+            print('\t\treturns both by default.\n')
+            print('\tdtl [file] add [description]')
+            print('\t\tAdds an entry to the given file with the given description')
+            print('\t\tand the current time as its timestamp.\n')
+            print('\tdtl [file] begin [description]')
+            print('\t\tSame as dtl add, but marks the entry as ongoing.\n')
+            print('\tdtl [file] end [description]')
+            print('\t\tCloses an ongoing entry in the given file with the given description.\n')
+            print('\tdtl [file] create')
+            print('\t\tCreates the file <file>. Useful for creating files in the DTL_dir directory.')
+            print()
+            print('Flags:')
+            print('\t--help')
+            print('\t\tShow this message.')
+            print('\t\tIf a command is specified: show more information about that command.\n')
 
-		case _:
-			print(f'Unknown command "{cmd}". Type "dtl --help" for a list of commands.')
+        case _:
+            print(f'Unknown command "{cmd}". Type "dtl --help" for a list of commands.')
 
 def main() -> None:
-	flags    = [flag.lstrip('-') for flag in sys.argv[1:] if flag[0] == '-']
-	commands = [cmd              for cmd  in sys.argv[1:] if  cmd[0] != '-']
+    flags    = [flag.lstrip('-') for flag in sys.argv[1:] if flag[0] == '-']
+    commands = [cmd              for cmd  in sys.argv[1:] if  cmd[0] != '-']
 
-	cmd = None
-	file = None
-	match len(commands):
-		case 0:
-			args = commands
-		case 1:
-			cmd, *args = commands
-		case _:
-			file, cmd, *args = commands
+    cmd = None
+    file = None
+    match len(commands):
+        case 0:
+            args = commands
+        case 1:
+            cmd, *args = commands
+        case _:
+            file, cmd, *args = commands
 
-	if 'help' in flags or 'h' in flags:
-		help_cmd(file, cmd, args)
-		exit(0)
+    if 'help' in flags or 'h' in flags:
+        help_cmd(file, cmd, args)
+        exit(0)
 
-	if 'version' in flags:
-		print(f'DTL {VERSION}')
-		exit(0)
+    if 'version' in flags:
+        print(f'DTL {VERSION}')
+        exit(0)
 
-	match cmd:
-		case 'parse':
-			parse_cmd(file, args)
-		case 'format':
-			format_cmd(file, args)
-		case 'find':
-			find_cmd(file, args)
-		case 'add':
-			add_cmd(file, args)
-		case 'begin':
-			begin_cmd(file, args)
-		case 'end':
-			end_cmd(file, args)
-		case 'create':
-			create_cmd(file, args)
-		case _:
-			help_cmd(file, cmd, args)
+    match cmd:
+        case 'parse':
+            parse_cmd(file, args)
+        case 'format':
+            format_cmd(file, args)
+        case 'find':
+            find_cmd(file, args)
+        case 'add':
+            add_cmd(file, args)
+        case 'begin':
+            begin_cmd(file, args)
+        case 'end':
+            end_cmd(file, args)
+        case 'create':
+            create_cmd(file, args)
+        case _:
+            help_cmd(file, cmd, args)
 
 if __name__ == "__main__":
-	main()
+    main()
 
--- dtl/tokenize.py
+++ dtl/tokenize.py
@@ -3,118 +3,118 @@ import re
 from collections import deque
 
 class Token:
-	def __init__(self, tok_type: str, value = None) -> None:
-		self.type = tok_type
-		self.value = value
+    def __init__(self, tok_type: str, value = None) -> None:
+        self.type = tok_type
+        self.value = value
 
 class Lexer:
-	def __init__(self, debug: str = False) -> None:
-		self.tokens = deque()
-		self.debug = debug
+    def __init__(self, debug: str = False) -> None:
+        self.tokens = deque()
+        self.debug = debug
 
-	def tokenize(self, src: str) -> None:
-		types = {
-			'AT'       : r'@',
-			'YEAR'     : r'\d{4}',
-			'MONTH'    : r'January|February|March|April|May|June|July|August|September|October|November|December',
-			'DATE'     : r'([2-3]?1st|2?2nd|2?3rd|[1-2]?[3-9]th|[1-3]0th|11th|12th|13th)',
-			'DAY'      : r'Mon(day)|Tue(sday)|Wed(esday)|Thu(rsday)|Fri(day)|Sat(urday)|Sun(day)',
-			'TIME'     : r'\d?\d:\d\d',
-			'DURATION' : r'([0-9]+\s(seconds|minutes|hours))|(second|minute|hour)',
-			'CMD'      : r'![A-Za-z]+',
-			'OPTION'   : r'#[A-Za-z]+',
-			'DESC'     : r'\[(?P<val>[^\]]*)\]',
-			'FOR'      : r'for',
-			'COLON'    : r':',
-			'ONGOING'  : r'\.\.\.',
-			'PERIOD'   : r'-',
-			'NL'       : r'\n+',
-			'TAB'      : r'\t',
-			'WS'       : r'\s',
-			'ERROR'    : r'.',
-		}
+    def tokenize(self, src: str) -> None:
+        types = {
+            'AT'       : r'@',
+            'YEAR'     : r'\d{4}',
+            'MONTH'    : r'January|February|March|April|May|June|July|August|September|October|November|December',
+            'DATE'     : r'([2-3]?1st|2?2nd|2?3rd|[1-2]?[3-9]th|[1-3]0th|11th|12th|13th)',
+            'DAY'      : r'Mon(day)|Tue(sday)|Wed(esday)|Thu(rsday)|Fri(day)|Sat(urday)|Sun(day)',
+            'TIME'     : r'\d?\d:\d\d',
+            'DURATION' : r'([0-9]+\s(seconds|minutes|hours))|(second|minute|hour)',
+            'CMD'      : r'![A-Za-z]+',
+            'OPTION'   : r'#[A-Za-z]+',
+            'DESC'     : r'\[(?P<val>[^\]]*)\]',
+            'FOR'      : r'for',
+            'COLON'    : r':',
+            'ONGOING'  : r'\.\.\.',
+            'PERIOD'   : r'-',
+            'NL'       : r'\n+',
+            'TAB'      : r'\t',
+            'WS'       : r'\s',
+            'ERROR'    : r'.',
+        }
 
-		for tok_type, pattern in types.items():
-			types[tok_type] = re.compile(pattern)
+        for tok_type, pattern in types.items():
+            types[tok_type] = re.compile(pattern)
 
-		self.tokens = deque()
+        self.tokens = deque()
 
-		at_line_start = True
-		line_indent = 0
-		prev_line_indent = 0
+        at_line_start = True
+        line_indent = 0
+        prev_line_indent = 0
 
-		pos = 0
-		while pos < len(src):
-			token_type = "ERROR"
-			token_val = ""
-			for tok_type, pattern in types.items():
-				match = pattern.match(src[pos:])
-				if match:
-					token_type = tok_type
-					if 'val' in match.groupdict():
-						token_val = match.group('val')
-					else:
-						token_val = match.group(0)
-					pos += len(match.group(0))
-					break
+        pos = 0
+        while pos < len(src):
+            token_type = "ERROR"
+            token_val = ""
+            for tok_type, pattern in types.items():
+                match = pattern.match(src[pos:])
+                if match:
+                    token_type = tok_type
+                    if 'val' in match.groupdict():
+                        token_val = match.group('val')
+                    else:
+                        token_val = match.group(0)
+                    pos += len(match.group(0))
+                    break
 
-			if at_line_start and token_type != 'TAB':
-				if line_indent > prev_line_indent:
-					for _ in range(line_indent-prev_line_indent):
-						self.tokens.append(Token('OPEN'))
-				elif line_indent < prev_line_indent:
-					for _ in range(prev_line_indent-line_indent):
-						self.tokens.append(Token('END'))
+            if at_line_start and token_type != 'TAB':
+                if line_indent > prev_line_indent:
+                    for _ in range(line_indent-prev_line_indent):
+                        self.tokens.append(Token('OPEN'))
+                elif line_indent < prev_line_indent:
+                    for _ in range(prev_line_indent-line_indent):
+                        self.tokens.append(Token('END'))
 
-			match token_type:
-				case 'NL':
-					at_line_start = True
-					prev_line_indent = line_indent
-					line_indent = 0
+            match token_type:
+                case 'NL':
+                    at_line_start = True
+                    prev_line_indent = line_indent
+                    line_indent = 0
 
-					self.tokens.append(Token('NL'))
-				case 'TAB':
-					if at_line_start:
-						line_indent += 1
-				case 'WS':
-					continue
-				case 'ERROR':
-					print(f'Error: unexpected charachter "{token_val}"')
-					at_line_start = False
-					self.tokens.append(Token(token_type, token_val))
-				case _:
-					at_line_start = False
-					self.tokens.append(Token(token_type, token_val))
+                    self.tokens.append(Token('NL'))
+                case 'TAB':
+                    if at_line_start:
+                        line_indent += 1
+                case 'WS':
+                    continue
+                case 'ERROR':
+                    print(f'Error: unexpected charachter "{token_val}"')
+                    at_line_start = False
+                    self.tokens.append(Token(token_type, token_val))
+                case _:
+                    at_line_start = False
+                    self.tokens.append(Token(token_type, token_val))
 
-		for _ in range(prev_line_indent):
-			self.tokens.append(Token('END'))
+        for _ in range(prev_line_indent):
+            self.tokens.append(Token('END'))
 
-		if self.debug:
-			print(' '.join([t.type for t in self.tokens]))
+        if self.debug:
+            print(' '.join([t.type for t in self.tokens]))
 
-	def peak(self) -> Token:
-		if len(self.tokens) > 0:
-			return self.tokens[0]
-		else:
-			return Token('EOF')
+    def peak(self) -> Token:
+        if len(self.tokens) > 0:
+            return self.tokens[0]
+        else:
+            return Token('EOF')
 
-	def pop(self) -> Token:
-		if self.debug:
-			print(self.peak().type)
+    def pop(self) -> Token:
+        if self.debug:
+            print(self.peak().type)
 
-		if len(self.tokens) > 0:
-			return self.tokens.popleft()
-		else:
-			return Token('EOF')
+        if len(self.tokens) > 0:
+            return self.tokens.popleft()
+        else:
+            return Token('EOF')
 
-	def assert_token(self, tok_t: str) -> Token|None:
-		if not isinstance(tok_t, list):
-			tok_t = [tok_t]
+    def assert_token(self, tok_t: str) -> Token|None:
+        if not isinstance(tok_t, list):
+            tok_t = [tok_t]
 
-		token = self.peak()
+        token = self.peak()
 
-		if not token.type in tok_t:
-			print(f'Error: expected {", ".join(tok_t)}, found {token.type}')
-			return None
-		else:
-			return self.pop()
+        if not token.type in tok_t:
+            print(f'Error: expected {", ".join(tok_t)}, found {token.type}')
+            return None
+        else:
+            return self.pop()
